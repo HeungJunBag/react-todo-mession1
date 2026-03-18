eb817d0229dd5ba76e0f2d3a0ec293c914d04942
HeungJunBag <p2005hihi@gmail.com>
2026-03-18 17:17:21 +0900

    등록 폼 입력 후 초기화 기능 추가

--- src/components/TodoWriteForm.jsx
+++ src/components/TodoWriteForm.jsx
@@ -1,24 +1,35 @@
+import { useState } from 'react'
 import { useTodos } from '../context/TodoContext'
 
 function TodoWriteForm() {
+    const [text, setText] = useState('')
     const { addTodo } = useTodos()
 
     const handleOnSubmit = (e) => {
         e.preventDefault()
         const form = e.target
 
-        if (form.todo.value.length == 0) {
+        if (form.todo.value.trim().length == 0) {
             alert('할일을 입력해주세요.')
             return
         }
 
         addTodo(form.todo.value)
+
+        form.reset()
+
+        setText('')
     }
 
     return (
         <>
             <form onSubmit={handleOnSubmit}>
-                <input type="text" name="todo" placeholder="할 일을 입력하세요." />
+                <input
+                    type="text"
+                    name="todo"
+                    placeholder="할 일을 입력하세요."
+                    onChange={(e) => setText(e.target.value)}
+                />
                 <button type="submit">등록</button>
             </form>
         </>
