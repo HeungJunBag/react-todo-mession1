06bd166776d12d2a6365d00fe44ca92abf8ceaa0
HeungJunBag <p2005hihi@gmail.com>
2026-03-18 15:04:46 +0900

    useRef 사용해서 아이디 값 증가

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 function App() {
-    const [todoId, setTodoId] = useState(4)
+    const lastId = useRef(4)
 
     const [todos, setTodos] = useState([
         { id: 3, text: '공부하기', checked: true },
         { id: 2, text: '코딩하기', checked: false },
         { id: 1, text: '운동하기', checked: true },
     ])
+
     const handleOnSubmit = (e) => {
         e.preventDefault()
         const form = e.target
-        setTodos([{ id: todoId, text: form.todo.value, checked: false }, ...todos])
-        setTodoId(todoId + 1)
+        setTodos([{ id: lastId.current, text: form.todo.value, checked: false }, ...todos])
+        lastId.current++
     }
 
     const removeTodo = (seletedId) => {
